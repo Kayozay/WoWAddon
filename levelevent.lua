6afe181206193931d2f64c32ac4cffcf57dc9b00
Kayozay <37458883+Kayozay@users.noreply.github.com>
2018-07-15 18:27:10 -0400

    Update levelevent.lua

--- levelevent.lua
+++ levelevent.lua
@@ -2,4 +2,4 @@ local Congrats_EventFrame = CreateFrame("Frame")
 Congrats_EventFrame:RegisterEvent("PLAYER_LEAVE_COMBAT")
 Congrats_EventFrame:SetScript("OnEvent",
     function(self, event, ...)  
-  
+  message('HelloWorld')
